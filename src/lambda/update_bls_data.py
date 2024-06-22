eec3af219322822e8605cd2fc15a43294bb48918
Sam Foster <slfotg@gmail.com>
2024-06-22 20:05:02 +0000

    Updated lambda and tf config

--- src/lambda/update_bls_data.py
+++ src/lambda/update_bls_data.py
@@ -1,17 +1,52 @@
 import json
 import logging
 import os
+from urllib.parse import urljoin
 
 import boto3
+import requests
+from bs4 import BeautifulSoup
 
 logger = logging.getLogger()
+
+BUCKET_NAME = os.environ["BUCKET_NAME"]
+BASE_URL = os.environ["BASE_URL"]
+USER_AGENT = os.environ["USER_AGENT"]
+HEADERS = {"User-Agent": USER_AGENT}
+
+
+def get_public_data() -> dict[str, object]:
+    with requests.get(BASE_URL, headers=HEADERS) as response:
+        response.raise_for_status()
+        metadata = dict()
+        soup = BeautifulSoup(response.content, features="html.parser")
+        for element in soup.find_all("a")[1:]:
+            url = urljoin(BASE_URL, element["href"])
+            with requests.head(url, headers=HEADERS) as header_request:
+                header_request.raise_for_status
+                metadata[element.text] = {
+                    "url": url,
+                    "Last-Modified": header_request.headers["Last-Modified"],
+                }
+        return metadata
+
+
+def download_to_s3(file_name: str, url: str, last_modified: str, s3_client):
+    with requests.get(url, headers=HEADERS) as response:
+        response.raise_for_status()
+        content = response.content
+        s3_client.put_object(
+            Body=content,
+            Bucket=BUCKET_NAME,
+            Key=file_name,
+            Metadata={"Last-Modified": last_modified},
+        )
 
 
 def get_metadata(client):
     try:
         response = client.get_object(
             Bucket=os.environ["BUCKET_NAME"],
-            Key=os.environ["METADATA_KEY"],
         )
         return json.loads(response["Body"].read())
     except client.exceptions.NoSuchKey:
@@ -22,5 +57,19 @@ def get_metadata(client):
 def update_bls_data(event, context):
 
     s3_client = boto3.client("s3")
+    for file_name, meta in get_public_data().items():
+        download_to_s3(file_name, meta["url"], meta["Last-Modified"], s3_client)
 
-    logger.info(get_metadata(s3_client))
+
+# s3_client = boto3.client("s3")
+# bucket = "github-slfotg-rearc-data"
+# response = s3_client.list_objects_v2(Bucket=os.environ["BUCKET_NAME"])
+# # Bucket=bucket, Key="requirements.txt", Body=b"test", ContentType="text/plain", Metadata={"created": "Jun 22 2024"})
+
+# for object in response["Contents"]:
+
+#     attrs = s3_client.head_object(
+#         Bucket=os.environ["BUCKET_NAME"], Key=object["Key"])
+#     print(object["Key"], attrs["Metadata"])
+
+# print(response["ResponseMetadata"])
--- terraform/main.tf
+++ terraform/main.tf
@@ -11,5 +11,72 @@ module "lambda_function" {
   handler       = "update_bls_data.update_bls_data"
   runtime       = "python3.12"
 
-  source_path = "../src/lambda"
+  source_path = "${path.module}../src/lambda"
+
+  store_on_s3 = true
+  s3_bucket   = module.s3_bucket.s3_bucket_id
+  s3_prefix   = "lambda-builds/"
+
+  artifacts_dir = "${path.root}/.terraform/lambda-builds/"
+
+  layers = [
+    module.lambda_layer_s3.lambda_layer_arn,
+  ]
+
+  environment_variables = {
+    BUCKET_NAME = "github-slfotg-rearc-data"
+    BASE_URL    = "https://download.bls.gov/pub/time.series/pr/"
+    USER_AGENT  = "slfotg@gmail.com"
+  }
+
+  attach_policy_jsons = true
+  policy_jsons = [
+    <<-EOT
+      {
+          "Version": "2012-10-17",
+          "Statement": [
+              {
+                  "Effect": "Allow",
+                  "Action": "s3:*",
+                  "Resource": "arn:aws:s3:::github-slfotg-rearc-data"
+              }
+          ]
+      }
+    EOT
+  ]
+  number_of_policy_jsons = 1
+
+}
+
+module "lambda_layer_s3" {
+  source = "terraform-aws-modules/lambda/aws"
+
+  create_layer = true
+
+  layer_name          = "${random_pet.this.id}-layer-s3"
+  description         = "update_bls_data layer"
+  compatible_runtimes = ["python3.12"]
+
+  source_path = "${path.module}../src/lambda"
+
+  store_on_s3 = true
+  s3_bucket   = module.s3_bucket.s3_bucket_id
+}
+
+resource "random_pet" "this" {
+  length = 2
+}
+
+module "s3_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "~> 3.0"
+
+  bucket_prefix = "${random_pet.this.id}-"
+  force_destroy = true
+
+  # S3 bucket-level Public Access Block configuration
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
