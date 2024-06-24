195e3e746e916d4e7668da075a14b3be611897e8
Sam Foster <slfotg@gmail.com>
2024-06-24 00:19:12 +0000

    added outputs

---
+++ terraform/update_data/outputs.tf
@@ -0,0 +1,60 @@
+# Lambda Function
+output "lambda_function_arn" {
+  description = "The ARN of the Lambda Function"
+  value       = module.lambda_function.lambda_function_arn
+}
+
+output "lambda_function_arn_static" {
+  description = "The static ARN of the Lambda Function. Use this to avoid cycle errors between resources (e.g., Step Functions)"
+  value       = module.lambda_function.lambda_function_arn_static
+}
+
+output "lambda_function_invoke_arn" {
+  description = "The Invoke ARN of the Lambda Function"
+  value       = module.lambda_function.lambda_function_invoke_arn
+}
+
+output "lambda_function_name" {
+  description = "The name of the Lambda Function"
+  value       = module.lambda_function.lambda_function_name
+}
+
+output "lambda_function_qualified_arn" {
+  description = "The ARN identifying your Lambda Function Version"
+  value       = module.lambda_function.lambda_function_qualified_arn
+}
+
+output "lambda_function_version" {
+  description = "Latest published version of Lambda Function"
+  value       = module.lambda_function.lambda_function_version
+}
+
+output "lambda_function_last_modified" {
+  description = "The date Lambda Function resource was last modified"
+  value       = module.lambda_function.lambda_function_last_modified
+}
+
+output "lambda_function_kms_key_arn" {
+  description = "The ARN for the KMS encryption key of Lambda Function"
+  value       = module.lambda_function.lambda_function_kms_key_arn
+}
+
+output "lambda_function_source_code_hash" {
+  description = "Base64-encoded representation of raw SHA-256 sum of the zip file"
+  value       = module.lambda_function.lambda_function_source_code_hash
+}
+
+output "lambda_function_source_code_size" {
+  description = "The size in bytes of the function .zip file"
+  value       = module.lambda_function.lambda_function_source_code_size
+}
+
+output "lambda_role_arn" {
+  description = "The ARN of the IAM role created for the Lambda Function"
+  value       = module.lambda_function.lambda_role_arn
+}
+
+output "lambda_role_name" {
+  description = "The name of the IAM role created for the Lambda Function"
+  value       = module.lambda_function.lambda_role_name
+}
